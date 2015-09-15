239d9d494ae38f30074c085c3d55a5b482834985
Piotr Jamroz <pm.jamroz@gmail.com>
2015-09-15 22:23:55 +0100

    indent with spaces

--- off.js
+++ off.js
@@ -1,176 +1,176 @@
 (function (root, factory) {
-	if (typeof define === "function" && define.amd) {
-		define(function () {
-			return (root.off = factory());
-		});
-	} else if (typeof module === "object" && module.exports) {
-		module.exports = (root.off = factory());
-	} else {
-		root.off = factory();
-	}
+    if (typeof define === "function" && define.amd) {
+        define(function () {
+            return (root.off = factory());
+        });
+    } else if (typeof module === "object" && module.exports) {
+        module.exports = (root.off = factory());
+    } else {
+        root.off = factory();
+    }
 })(this, function () {
-	var off = function (func, context) {
-		var _handlers = [],
-			_before = [];
+    var off = function (func, context) {
+        var _handlers = [],
+            _before = [];
 
-		func = func || function () {};
+        func = func || function () {};
 
-		var runner = function () {
-			var self = context || this,
-				args, blocked, result;
+        var runner = function () {
+            var self = context || this,
+                args, blocked, result;
 
-			args = Array.prototype.slice.call(arguments, 0);
+            args = Array.prototype.slice.call(arguments, 0);
 
-			blocked = _before.some(function (handler) {
-				return handler.apply(this, args);
-			});
+            blocked = _before.some(function (handler) {
+                return handler.apply(this, args);
+            });
 
-			if (blocked) {
-				return;
-			}
+            if (blocked) {
+                return;
+            }
 
-			result = runner.func.apply(self, args);
+            result = runner.func.apply(self, args);
 
-			if (runner.lock) {
-				runner.lock = false;
-			} else {
-				_handlers.forEach(function (handler) {
-					if (result instanceof Function && result.off && result.bind) {
-						result.bind(handler);
-					}
-					if (result instanceof Function && result._off) {
-						result.add(handler);
-					} else {
-						handler.call(self, result);
-					}
-				});
-			}
+            if (runner.lock) {
+                runner.lock = false;
+            } else {
+                _handlers.forEach(function (handler) {
+                    if (result instanceof Function && result.off && result.bind) {
+                        result.bind(handler);
+                    }
+                    if (result instanceof Function && result._off) {
+                        result.add(handler);
+                    } else {
+                        handler.call(self, result);
+                    }
+                });
+            }
 
-			return result;
-		};
+            return result;
+        };
 
-		runner.before = function (handler) {
-			if (_before.indexOf(handler) === -1) {
-				_before.push(handler);
-			}
-			return runner;
-		};
+        runner.before = function (handler) {
+            if (_before.indexOf(handler) === -1) {
+                _before.push(handler);
+            }
+            return runner;
+        };
 
-		runner.before.remove = function(handler) {
-			var index = _before.indexOf(handler);
-			if (index !== -1) {
-				_before.splice(index, 1);
-			}
-		};
+        runner.before.remove = function(handler) {
+            var index = _before.indexOf(handler);
+            if (index !== -1) {
+                _before.splice(index, 1);
+            }
+        };
 
-		runner.add = function (handler) {
-			if (_handlers.indexOf(handler) === -1) {
-				_handlers.push(handler);
-			}
-			return runner;
-		};
+        runner.add = function (handler) {
+            if (_handlers.indexOf(handler) === -1) {
+                _handlers.push(handler);
+            }
+            return runner;
+        };
 
-		runner.remove = function (handler) {
-			var index = _handlers.indexOf(handler);
-			if (index !== -1) {
-				_handlers.splice(index, 1);
-			}
-		};
+        runner.remove = function (handler) {
+            var index = _handlers.indexOf(handler);
+            if (index !== -1) {
+                _handlers.splice(index, 1);
+            }
+        };
 
-		runner.override = function(override) {
-			var func = runner.func;
-			runner.func = function() {
-				var args = Array.prototype.slice.call(arguments, 0);
-				args.unshift(func.bind(this));
-				return override.apply(this, args);
-			}
-		};
+        runner.override = function(override) {
+            var func = runner.func;
+            runner.func = function() {
+                var args = Array.prototype.slice.call(arguments, 0);
+                args.unshift(func.bind(this));
+                return override.apply(this, args);
+            }
+        };
 
-		runner._scopes = {};
+        runner._scopes = {};
 
-		runner.as = function(name) {
-			runner._scopes[name] = runner._scopes[name] || off(runner);
-			return runner._scopes[name];
-		};
+        runner.as = function(name) {
+            runner._scopes[name] = runner._scopes[name] || off(runner);
+            return runner._scopes[name];
+        };
 
-		runner._off = true;
+        runner._off = true;
 
-		runner.func = func;
+        runner.func = func;
 
-		return runner;
-	};
+        return runner;
+    };
 
-	off.signal = function () {
-		var last = undefined,
-			triggered = false,
-			result;
-		result = off(function (value) {
-			triggered = true;
-			last = value;
-			return value;
-		});
-		result.bind = function() {
-			result.add(handler);
-			if (triggered) {
-				handler(last);
-			}
-		};
-		return result;
-	};
+    off.signal = function () {
+        var last = undefined,
+            triggered = false,
+            result;
+        result = off(function (value) {
+            triggered = true;
+            last = value;
+            return value;
+        });
+        result.bind = function() {
+            result.add(handler);
+            if (triggered) {
+                handler(last);
+            }
+        };
+        return result;
+    };
 
-	off.property = function (setget) {
-		if (typeof setget !== "function") {
-			var _v = setget;
-			setget = function(v) {
-				return arguments.length === 1 ? _v = v : _v;
-			}
-		}
+    off.property = function (setget) {
+        if (typeof setget !== "function") {
+            var _v = setget;
+            setget = function(v) {
+                return arguments.length === 1 ? _v = v : _v;
+            }
+        }
 
-		var property = off(function(value){
-			if (arguments.length === 1 && setget() !== value) {
-				return setget(value);
-			}
-			else {
-				property.lock = true;
-				return setget();
-			}
-		});
+        var property = off(function(value){
+            if (arguments.length === 1 && setget() !== value) {
+                return setget(value);
+            }
+            else {
+                property.lock = true;
+                return setget();
+            }
+        });
 
-		property.setget = setget;
+        property.setget = setget;
 
-		property.bind = function(handler) {
-			property.add(handler);
-			if (setget() !== undefined) {
-				handler(setget());
-			}
-		};
+        property.bind = function(handler) {
+            property.add(handler);
+            if (setget() !== undefined) {
+                handler(setget());
+            }
+        };
 
-		property._property = true;
-		return property;
-	};
+        property._property = true;
+        return property;
+    };
 
-	off.async = function (func) {
-		var last_callback = null;
-		return off(function () {
-			var callback;
-			if (last_callback) {
-				last_callback.lock = true;
-			}
-			last_callback = callback = off(function (value) {return value}, this);
-			var args = Array.prototype.slice.call(arguments, 0).concat(callback);
-			func.apply(this, args);
-			return callback;
-		});
-	};
+    off.async = function (func) {
+        var last_callback = null;
+        return off(function () {
+            var callback;
+            if (last_callback) {
+                last_callback.lock = true;
+            }
+            last_callback = callback = off(function (value) {return value}, this);
+            var args = Array.prototype.slice.call(arguments, 0).concat(callback);
+            func.apply(this, args);
+            return callback;
+        });
+    };
 
-	off.decorate = function (obj) {
-		for (var property in obj) {
-			if (typeof (obj[property]) === "function" && ! (obj[property]._off)) {
-				obj[property] = off(obj[property]);
-			}
-		}
-		return obj;
-	};
+    off.decorate = function (obj) {
+        for (var property in obj) {
+            if (typeof (obj[property]) === "function" && ! (obj[property]._off)) {
+                obj[property] = off(obj[property]);
+            }
+        }
+        return obj;
+    };
 
-	return off;
+    return off;
 });
--- spec/offSpec.js
+++ spec/offSpec.js
@@ -1,345 +1,345 @@
 describe('off', function () {
 
-	var off = require('../off'),
-		handler,
-		fn,
-		async;
-
-	beforeEach(function () {
-		handler = jasmine.createSpy();
-		fn = off(function () {});
-		async = off.async(function (callback) {
-			setTimeout(callback, 10);
-		});
-
-	});
-
-	it('should run handlers after running wrapped function', function () {
-		fn.add(handler);
-		expect(handler).not.toHaveBeenCalled();
-
-		fn();
-		expect(handler).toHaveBeenCalled();
-	});
-
-	it('should remove handlers', function () {
-		fn.add(handler);
-		expect(handler).not.toHaveBeenCalled();
-
-		fn();
-		expect(handler).toHaveBeenCalled();
-
-		fn.remove(handler);
-		fn();
-		expect(handler.calls.count()).toEqual(1);
-	});
-
-	it('should run handlers for async calls', function (done) {
-		async.add(handler);
-
-		async();
-
-		setTimeout(function () {
-			expect(handler).toHaveBeenCalled();
-			done();
-		}, 20);
-
-	});
-
-	it('should lock handlers if function is locked', function () {
-		var locker = off(function (value) {
-			if (value) {
-				locker.lock = true;
-			}
-		});
-
-		locker.add(handler);
-
-		locker(false);
-		locker(true);
-
-		expect(handler.calls.count()).toEqual(1);
-	});
-
-	it('should lock handlers if async function is locked', function (done) {
-		var async_locker = off(function (value) {
-			var result_callback = off.signal();
-			if (value) {
-				async_locker.lock = true;
-			}
-			setTimeout(result_callback, 10);
-			return result_callback;
-		});
-
-		async_locker.add(handler);
-
-		async_locker(false);
-		async_locker(true);
-
-		setTimeout(function () {
-			expect(handler.calls.count()).toEqual(1);
-			done();
-		}, 40);
-	});
-
-	it('should not allow to add same handler multiple times', function () {
-		fn.add(handler);
-		fn.add(handler);
-
-		fn();
-		expect(handler.calls.count()).toEqual(1);
-
-	});
-
-	it('should correctly handle context of the sync handler', function () {
-		var context = {
-			action: fn,
-			handler: function () {
-				expect(this).toEqual(context);
-			}
-		};
-
-		context.action.add(context.handler);
-		context.action();
-	});
-
-	it('should correctly handle context of the async handler', function (done) {
-		var context = {
-			action: async,
-			handler: function () {
-				expect(this).toEqual(context);
-				done();
-			}
-		};
-
-		context.action.add(context.handler);
-		context.action();
-	});
-
-	it('should allow to add a before handler', function () {
-		var result = '';
-		var before = function () {
-			result += 'a';
-		};
-		var action = off(function () {
-			result += 'b';
-		});
-		var after = function () {
-			result += 'c';
-		};
-
-		action.add(after);
-		action.before(before);
-
-		action();
-
-		expect(result).toEqual("abc");
-	});
-
-	it('should allow to lock an action', function () {
-		var result = '';
-		var before = function () {
-			result += 'a';
-			return true;
-		};
-		var action = off(function () {
-			result += 'b';
-		});
-		var after = function () {
-			result += 'c';
-		};
-
-		action.add(after);
-		action.before(before);
-
-		action();
-
-		expect(result).toEqual("a");
-	});
-
-	it('should decorate all object methods', function() {
-		var test = function() {};
-
-		var obj = {
-			foo: 'bar',
-			test: test
-		};
-
-		var obj = off.decorate(obj);
-
-		expect(obj.foo).toEqual('bar');
-		expect(obj.test).not.toBe(test);
-		expect(obj.test._off).toBe(true);
-	});
-
-	it('should not decorate already wrapped methods', function() {
-		var wrapped_test = off(function(){});
-
-		var obj = {
-			wrapped_test: wrapped_test
-		};
-
-		obj = off.decorate(obj);
-		expect(obj.wrapped_test).toBe(wrapped_test);
-	});
-
-	it('should allow to override functions', function(){
-		var base = {
-			create_text: off(function(value){
-				this.text = value;
-				return value;
-			})
-		};
-
-		var foo = Object.create(base);
-		foo.create_text.override(function($super, value){
-			this.text = 'foo: ' + $super(value);
-			return this.text;
-		});
-
-		var bar = Object.create(foo);
-		bar.create_text.override(function($super, value){
-			this.text = $super(value) + '!';
-			return this.text;
-		});
-
-		bar.create_text('test');
-		expect(bar.text).toBe('foo: test!');
-	});
-
-	it('should pass result and args to handlers', function() {
-		var method = off(function(a) {
-				return a + 1;
-			});
-
-		method.add(handler);
-		method(1);
-
-		expect(handler).toHaveBeenCalledWith(2);
-	});
-
-	describe('signal', function () {
+    var off = require('../off'),
+        handler,
+        fn,
+        async;
+
+    beforeEach(function () {
+        handler = jasmine.createSpy();
+        fn = off(function () {});
+        async = off.async(function (callback) {
+            setTimeout(callback, 10);
+        });
+
+    });
+
+    it('should run handlers after running wrapped function', function () {
+        fn.add(handler);
+        expect(handler).not.toHaveBeenCalled();
+
+        fn();
+        expect(handler).toHaveBeenCalled();
+    });
+
+    it('should remove handlers', function () {
+        fn.add(handler);
+        expect(handler).not.toHaveBeenCalled();
+
+        fn();
+        expect(handler).toHaveBeenCalled();
+
+        fn.remove(handler);
+        fn();
+        expect(handler.calls.count()).toEqual(1);
+    });
+
+    it('should run handlers for async calls', function (done) {
+        async.add(handler);
+
+        async();
+
+        setTimeout(function () {
+            expect(handler).toHaveBeenCalled();
+            done();
+        }, 20);
+
+    });
+
+    it('should lock handlers if function is locked', function () {
+        var locker = off(function (value) {
+            if (value) {
+                locker.lock = true;
+            }
+        });
+
+        locker.add(handler);
+
+        locker(false);
+        locker(true);
+
+        expect(handler.calls.count()).toEqual(1);
+    });
+
+    it('should lock handlers if async function is locked', function (done) {
+        var async_locker = off(function (value) {
+            var result_callback = off.signal();
+            if (value) {
+                async_locker.lock = true;
+            }
+            setTimeout(result_callback, 10);
+            return result_callback;
+        });
+
+        async_locker.add(handler);
+
+        async_locker(false);
+        async_locker(true);
+
+        setTimeout(function () {
+            expect(handler.calls.count()).toEqual(1);
+            done();
+        }, 40);
+    });
+
+    it('should not allow to add same handler multiple times', function () {
+        fn.add(handler);
+        fn.add(handler);
+
+        fn();
+        expect(handler.calls.count()).toEqual(1);
+
+    });
+
+    it('should correctly handle context of the sync handler', function () {
+        var context = {
+            action: fn,
+            handler: function () {
+                expect(this).toEqual(context);
+            }
+        };
+
+        context.action.add(context.handler);
+        context.action();
+    });
+
+    it('should correctly handle context of the async handler', function (done) {
+        var context = {
+            action: async,
+            handler: function () {
+                expect(this).toEqual(context);
+                done();
+            }
+        };
+
+        context.action.add(context.handler);
+        context.action();
+    });
+
+    it('should allow to add a before handler', function () {
+        var result = '';
+        var before = function () {
+            result += 'a';
+        };
+        var action = off(function () {
+            result += 'b';
+        });
+        var after = function () {
+            result += 'c';
+        };
+
+        action.add(after);
+        action.before(before);
+
+        action();
+
+        expect(result).toEqual("abc");
+    });
+
+    it('should allow to lock an action', function () {
+        var result = '';
+        var before = function () {
+            result += 'a';
+            return true;
+        };
+        var action = off(function () {
+            result += 'b';
+        });
+        var after = function () {
+            result += 'c';
+        };
+
+        action.add(after);
+        action.before(before);
+
+        action();
+
+        expect(result).toEqual("a");
+    });
+
+    it('should decorate all object methods', function() {
+        var test = function() {};
+
+        var obj = {
+            foo: 'bar',
+            test: test
+        };
+
+        var obj = off.decorate(obj);
+
+        expect(obj.foo).toEqual('bar');
+        expect(obj.test).not.toBe(test);
+        expect(obj.test._off).toBe(true);
+    });
+
+    it('should not decorate already wrapped methods', function() {
+        var wrapped_test = off(function(){});
+
+        var obj = {
+            wrapped_test: wrapped_test
+        };
+
+        obj = off.decorate(obj);
+        expect(obj.wrapped_test).toBe(wrapped_test);
+    });
+
+    it('should allow to override functions', function(){
+        var base = {
+            create_text: off(function(value){
+                this.text = value;
+                return value;
+            })
+        };
+
+        var foo = Object.create(base);
+        foo.create_text.override(function($super, value){
+            this.text = 'foo: ' + $super(value);
+            return this.text;
+        });
+
+        var bar = Object.create(foo);
+        bar.create_text.override(function($super, value){
+            this.text = $super(value) + '!';
+            return this.text;
+        });
+
+        bar.create_text('test');
+        expect(bar.text).toBe('foo: test!');
+    });
+
+    it('should pass result and args to handlers', function() {
+        var method = off(function(a) {
+                return a + 1;
+            });
+
+        method.add(handler);
+        method(1);
+
+        expect(handler).toHaveBeenCalledWith(2);
+    });
+
+    describe('signal', function () {
 
-		var signal;
+        var signal;
 
-		beforeEach(function () {
-			signal = off.signal();
-		});
+        beforeEach(function () {
+            signal = off.signal();
+        });
 
-		it('should pass value passed when exeucting signal', function () {
-			signal.add(handler);
+        it('should pass value passed when exeucting signal', function () {
+            signal.add(handler);
 
-			signal('test');
+            signal('test');
 
-			expect(handler).toHaveBeenCalledWith('test');
+            expect(handler).toHaveBeenCalledWith('test');
 
-		});
+        });
 
-	});
+    });
 
-	describe('scopes', function() {
+    describe('scopes', function() {
 
-		it('should cache scoped methods', function() {
-			var base = off.signal(),
-				foo = base.as('foo'),
-				foo2 = base.as('foo'),
-				bar = base.as('bar');
-
-			expect(foo).toEqual(foo2);
-			expect(foo).not.toEqual(bar);
-		});
-
-		it('should create cascaded scopes', function() {
-			var base = off.signal(),
-				foo = base.as('foo'),
-				foobar = base.as('foo').as('bar');
-
-			var base_spy = jasmine.createSpy(),
-				foo_spy = jasmine.createSpy(),
-				foobar_spy = jasmine.createSpy();
-
-			base.add(base_spy);
-			foo.add(foo_spy);
-			foobar.add(foobar_spy);
-
-			base('BASE');
-			expect(base_spy).toHaveBeenCalledWith('BASE');
-			expect(foo_spy).not.toHaveBeenCalledWith('BASE');
-			expect(foobar_spy).not.toHaveBeenCalledWith('BASE');
-
-			foo('FOO');
-			expect(base_spy).toHaveBeenCalledWith('FOO');
-			expect(foo_spy).toHaveBeenCalledWith('FOO');
-			expect(foobar_spy).not.toHaveBeenCalledWith('FOO');
-
-			foobar('FOOBAR');
-			expect(base_spy).toHaveBeenCalledWith('FOOBAR');
-			expect(foo_spy).toHaveBeenCalledWith('FOOBAR');
-			expect(foobar_spy).toHaveBeenCalledWith('FOOBAR');
-		});
-
-	});
-
-	describe('property', function () {
-
-		var property;
-
-		beforeEach(function () {
-			property = off.property();
-		});
-
-		it('should store the last value', function () {
-			property(10);
-			expect(property()).toEqual(10);
-
-			property('test');
-			property('test2');
-			expect(property()).toEqual('test2');
-		});
-
-		it('should run handler only when setting the value', function () {
-			property.add(handler);
-
-			property(10);
-			property();
-
-			expect(handler.calls.count()).toEqual(1);
-		});
-
-		it('should run handlers only when value changes', function () {
-			property.add(handler);
-
-			property(10);
-			property(11);
-			property(11);
-
-			expect(handler.calls.count()).toEqual(2);
-		});
-
-		it('should run handler added with bind() if value is defined', function () {
-
-			property(20);
-
-			property.bind(handler);
-
-			expect(handler).toHaveBeenCalledWith(20);
-		});
-
-		it('should allow to create private/public properties', function(){
-			var _foo = off.property(),
-				foo = off.property(_foo);
-
-			foo.add(handler);
-			_foo(1);
-			expect(handler.calls.count()).toEqual(0);
-			expect(foo()).toEqual(1);
-			expect(_foo()).toEqual(1);
-
-			foo(2);
-			expect(handler.calls.count()).toEqual(1);
-			expect(foo()).toEqual(2);
-			expect(_foo()).toEqual(2);
-		});
-	});
+        it('should cache scoped methods', function() {
+            var base = off.signal(),
+                foo = base.as('foo'),
+                foo2 = base.as('foo'),
+                bar = base.as('bar');
+
+            expect(foo).toEqual(foo2);
+            expect(foo).not.toEqual(bar);
+        });
+
+        it('should create cascaded scopes', function() {
+            var base = off.signal(),
+                foo = base.as('foo'),
+                foobar = base.as('foo').as('bar');
+
+            var base_spy = jasmine.createSpy(),
+                foo_spy = jasmine.createSpy(),
+                foobar_spy = jasmine.createSpy();
+
+            base.add(base_spy);
+            foo.add(foo_spy);
+            foobar.add(foobar_spy);
+
+            base('BASE');
+            expect(base_spy).toHaveBeenCalledWith('BASE');
+            expect(foo_spy).not.toHaveBeenCalledWith('BASE');
+            expect(foobar_spy).not.toHaveBeenCalledWith('BASE');
+
+            foo('FOO');
+            expect(base_spy).toHaveBeenCalledWith('FOO');
+            expect(foo_spy).toHaveBeenCalledWith('FOO');
+            expect(foobar_spy).not.toHaveBeenCalledWith('FOO');
+
+            foobar('FOOBAR');
+            expect(base_spy).toHaveBeenCalledWith('FOOBAR');
+            expect(foo_spy).toHaveBeenCalledWith('FOOBAR');
+            expect(foobar_spy).toHaveBeenCalledWith('FOOBAR');
+        });
+
+    });
+
+    describe('property', function () {
+
+        var property;
+
+        beforeEach(function () {
+            property = off.property();
+        });
+
+        it('should store the last value', function () {
+            property(10);
+            expect(property()).toEqual(10);
+
+            property('test');
+            property('test2');
+            expect(property()).toEqual('test2');
+        });
+
+        it('should run handler only when setting the value', function () {
+            property.add(handler);
+
+            property(10);
+            property();
+
+            expect(handler.calls.count()).toEqual(1);
+        });
+
+        it('should run handlers only when value changes', function () {
+            property.add(handler);
+
+            property(10);
+            property(11);
+            property(11);
+
+            expect(handler.calls.count()).toEqual(2);
+        });
+
+        it('should run handler added with bind() if value is defined', function () {
+
+            property(20);
+
+            property.bind(handler);
+
+            expect(handler).toHaveBeenCalledWith(20);
+        });
+
+        it('should allow to create private/public properties', function(){
+            var _foo = off.property(),
+                foo = off.property(_foo);
+
+            foo.add(handler);
+            _foo(1);
+            expect(handler.calls.count()).toEqual(0);
+            expect(foo()).toEqual(1);
+            expect(_foo()).toEqual(1);
+
+            foo(2);
+            expect(handler.calls.count()).toEqual(1);
+            expect(foo()).toEqual(2);
+            expect(_foo()).toEqual(2);
+        });
+    });
 
 })
