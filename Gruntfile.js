4717baf4cb161cb0431d3d22d4f419532f714b91
Piotr Jamroz <pm.jamroz@gmail.com>
2015-09-15 22:24:35 +0100

    Gruntfile indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,25 @@
 module.exports = function (grunt) {
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-		shell: {
-			uglify: {
-				command: 'uglifyjs off.js --mangle --compress -o off-min.js'
-				
-			},
-			bumpup: {
-				command: 'mversion patch -m'
-			},
-			test: {
-				command: 'jasmine'
-			}
-		},
+        shell: {
+            uglify: {
+                command: 'uglifyjs off.js --mangle --compress -o off-min.js'
+                
+            },
+            bumpup: {
+                command: 'mversion patch -m'
+            },
+            test: {
+                command: 'jasmine'
+            }
+        },
 
 
-	});
+    });
 
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.registerTask('build', ['shell:test','shell:uglify']);
-	grunt.registerTask('bumpup', ['shell:bumpup']);
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.registerTask('build', ['shell:test','shell:uglify']);
+    grunt.registerTask('bumpup', ['shell:bumpup']);
 };
